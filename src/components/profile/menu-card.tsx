dc82c749a6f363c2d126f573c89ae3643a80c5c0
hlaing-dev <hlaingmin1235145@gmail.com>
2024-12-24 23:54:36 +0700

    update

--- src/components/profile/menu-card.tsx
+++ src/components/profile/menu-card.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useNavigate } from "react-router-dom";
 import { Card } from "../ui/card";
 
 const MenuCard = ({ Icon, title }: any) => {
+  const navigate = useNavigate();
   return (
-    <Card className="bg-[#181818] border-0 h-[76px] flex justify-center items-center flex-col gap-2 shadow-lg">
+    <Card onClick={()=>navigate('/wallet')} className="bg-[#181818] border-0 h-[76px] flex justify-center items-center flex-col gap-2 shadow-lg">
       <div className="">
         <Icon />
       </div>
