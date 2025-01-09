0074ac30eb3e31c96fe6b3f0f7a8e8efcd288ef2
ryanreynold21 <aunkyawin2035@gmail.com>
2025-01-10 01:13:08 +0700

    add_cmt_post

--- src/store/api/explore/exploreApi.tsx
+++ src/store/api/explore/exploreApi.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { RootState } from "@/store/store";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const exploreApi = createApi({
@@ -6,6 +7,13 @@ export const exploreApi = createApi({
   // baseQuery: fetchBaseQuery({ baseUrl: "http://107.148.47.94:8800/api/v1" }),
   baseQuery: fetchBaseQuery({
     baseUrl: "http://107.148.47.94:8800/api/v1",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).persist.user.token; // Adjust 'auth.token' to match your Redux slice structure
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getExploreHeader: builder.query<any, string>({
@@ -32,6 +40,35 @@ export const exploreApi = createApi({
         method: "GET",
       }),
     }),
+    postCommentExp: builder.mutation<
+    void,
+    { post_id: any; content: any; comment_id?: any; reply_id?: any }
+  >({
+    query: ({ post_id, content, comment_id, reply_id }) => {
+
+      const body: {
+        post_id: any;
+        content: any;
+        comment_id?: any;
+        reply_id?: any;
+      } = {
+        post_id,
+        content,
+      };
+
+      if (comment_id != null) {
+        body.comment_id = comment_id;
+      }
+      if (reply_id != null) {
+        body.reply_id = reply_id;
+      }
+      return {
+        url: `/post/comment`,
+        method: "POST",
+        body, 
+      };
+    },
+  }),
   }),
 });
 
@@ -39,5 +76,6 @@ export const {
   useGetExploreHeaderQuery,
   useGetExploreTagQuery,
   useGetApplicationAdsQuery,
-  useGetExploreListQuery
+  useGetExploreListQuery,
+  usePostCommentExpMutation
 } = exploreApi;
