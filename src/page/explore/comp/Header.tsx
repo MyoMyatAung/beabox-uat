46982e684fe1b228caff0f9560ae7f757fd9e7b3
ryanreynold21 <aunkyawin2035@gmail.com>
2025-01-24 15:34:21 +0700

    fix

--- src/page/explore/comp/Header.tsx
+++ src/page/explore/comp/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC<HeaderProps> = ({ activeTab, setActiveTab }) => {
   // console.log(hd)
 
   return (
-    <div className="bg-black z-[99] py-[5px] sticky top-0 w-screen overscroll-auto">
+    <div className="bg-[#16131C] px-2 z-[99] py-[5px] sticky top-0 w-screen overscroll-auto">
       {isLoading ? (
         <div className=" w-[330px] bg-white/20 rounded-md h-[50px] animate-pulse"></div>
       ) : (
