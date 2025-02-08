2a37c963a3b32241d4244d7affaa318d6738e9e4
minthiha26 <minthiha.flar@gmail.com>
2025-02-09 01:14:47 +0630

    fix

--- src/page/auth/Login.tsx
+++ src/page/auth/Login.tsx
@@ -205,9 +205,14 @@ const Login = () => {
                 {/* {isLoading ? "loading..." : "Login"} */}
                 {captchaLoading ? <SmallLoader /> : "登录"}
               </Button>
-              <Link to={paths.forgot_password}>
-                <p className="text-center text-[14px] mt-5">忘记密码？</p>
-              </Link>
+              <div className="flex justify-center">
+                <Link
+                  to={paths.forgot_password}
+                  className="text-center text-[14px] mt-5"
+                >
+                  忘记密码？
+                </Link>
+              </div>
             </div>
             <Dialog open={show验证码} onOpenChange={setShow验证码}>
               <DialogContent className="bg-[#16131C] border-0 shadow-lg rounded-lg max-w-[320px]">
