c00d6b043dec7e7d15714fc9a0e95608e23a9827
minthiha26 <minthiha.flar@gmail.com>
2025-02-09 04:43:11 +0630

    fix drawer

--- src/store/api/authApi.tsx
+++ src/store/api/authApi.tsx
@@ -134,13 +134,12 @@ export const authApi = createApi({
       query: ({ username, password, captcha, captcha_key }: any) => ({
         url: "/login",
         method: "POST",
-        // body: convertToSecurePayload({
-        //   username,
-        //   password,
-        //   captcha,
-        //   captcha_key,
-        // }),
-        body: { username, password, captcha, captcha_key },
+        body: convertToSecurePayload({
+          username,
+          password,
+          captcha,
+          captcha_key,
+        }),
       }),
     }),
     storeSecurityQues: builder.mutation<any, string>({
