d8fc28c6071eb788fc72d8c5d01f390128d8ca8c
minthiha26 <minthiha.flar@gmail.com>
2025-02-09 12:35:49 +0630

    fix ui

--- src/page/profile/Profile.tsx
+++ src/page/profile/Profile.tsx
@@ -79,7 +79,7 @@ const Profile = () => {
   if (isLoading) return <Loader />;
 
   return (
-    <>
+    <div className="h-[calc(100vh-76px)] overflow-hidden overflow-y-scroll hide-sb">
       <div className="gradient-overlay"></div>
       <img
         src={
@@ -101,7 +101,7 @@ const Profile = () => {
       ) : (
         ""
       )}
-      <div className={`z-[1200] max-h-screen hide-sb profile-bg `}>
+      <div className={`z-[1200]`}>
         {show ? (
           <div className="absolute top-0 z-[1500] left-0 w-full h-full mx-auto flex flex-col justify-center items-center bg-black/80">
             <div className="z-[1200] px-10">
@@ -261,7 +261,7 @@ const Profile = () => {
         </div>
 
         {/* <div ref={headerRef} className="sticky z-[1300] top-0 w-full"></div> */}
-        {showHeader ? (
+        {false ? (
           <ScrollHeader
             photo={data?.data?.profile_photo}
             name={data?.data?.nickname}
@@ -270,7 +270,7 @@ const Profile = () => {
           <></>
         )}
 
-        {showHeader ? (
+        {false ? (
           <div className={`sticky top-[80px] z-[1200]`}>
             <div className="z-[1200] relative px-5">
               <VideoTabs
@@ -290,7 +290,7 @@ const Profile = () => {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
