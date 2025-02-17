f4d0c82a1e7f98a7429f1f51c2d14e64e829100a
minthiha26 <minthiha.flar@gmail.com>
2025-02-17 09:32:12 +0630

    fix auth error

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,6 @@ const App = () => {
         <Landing />
       ) : (
         <>
-          {" "}
           <Routing />
           <Toaster />
           <ErrorToast />
--- src/components/profile/video-tabs.tsx
+++ src/components/profile/video-tabs.tsx
@@ -16,14 +16,18 @@ const VideoTabs = ({ login, showHeader, headerRef }: any) => {
   return (
     <Tabs defaultValue="upload" className="py-5">
       <TabsList className="grid w-full grid-cols-3 z-[1600] bg-transparent sticky top-[100px]">
-        <TabsTrigger
-          className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2"
-          value="upload"
-        >
-          <span className="flex items-center gap-1">
-            <Play /> 我的作品
-          </span>
-        </TabsTrigger>
+        {user?.token ? (
+          <TabsTrigger
+            className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2"
+            value="upload"
+          >
+            <span className="flex items-center gap-1">
+              <Play /> 我的作品
+            </span>
+          </TabsTrigger>
+        ) : (
+          <></>
+        )}
         <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2"
           value="liked"
