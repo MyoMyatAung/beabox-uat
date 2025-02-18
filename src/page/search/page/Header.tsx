1ef0d7ec7991f9ec7126237c13e995ddb5822913
Htetaungkyaw <htetaung200071@gmail.com>
2025-02-18 22:27:27 +0630

    fix

--- src/page/search/page/Header.tsx
+++ src/page/search/page/Header.tsx
@@ -33,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({
           </span>
 
           <span
-            className={`h-[5px] w-[25px] rounded-md bg-white ${
+            className={`h-[3px] w-[25px] rounded-md bg-[#CD3EFF] ${
               activeTab?.name !== tt?.name ? " opacity-0" : "opacity-100"
             }`}
           ></span>
