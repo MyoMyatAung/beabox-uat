3bad3337b260173d7935960b96ebddd95c6142a6
minthiha26 <minthiha.flar@gmail.com>
2025-02-19 22:16:13 +0630

    fix btn & scroll

--- src/components/profile/setting-btn.tsx
+++ src/components/profile/setting-btn.tsx
@@ -63,7 +63,7 @@ const SettingBtn = ({ setShow }: any) => {
           <Menu />
         </button>
       </DrawerTrigger>
-      <DrawerContent className="border-0 bg-[#121012] z-[1600]">
+      <DrawerContent className="border-0 bg-[#121012] z-[3600]">
         <div className="w-full px-5 py-7">
           <div className="space-y-3">
             {(user?.token ? data : data2)?.map(
