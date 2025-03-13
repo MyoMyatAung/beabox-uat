9988609673e91c21db7181ac9c7e6d98f4f4ec3f
minthiha26 <minthiha.flar@gmail.com>
2025-03-13 13:47:48 +0630

    fix

--- src/components/profile/video-tab2.tsx
+++ src/components/profile/video-tab2.tsx
@@ -11,7 +11,7 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
   const defaultTab2 = useSelector((state: any) => state.persist.defaultTab2);
   const dispatch = useDispatch();
   return (
-    <Tabs defaultValue={"liked"} className="my-5">
+    <Tabs defaultValue={defaultTab2 ? defaultTab2 : "video"} className="my-5">
       <TabsList className="grid w-full grid-cols-3 z-[1600] bg-transparent sticky top-[100px] px-5">
         <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2 "
@@ -19,7 +19,7 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
           onClick={() => dispatch(setDefaultTab2("video"))}
         >
           <span className="flex items-center gap-1">
-            <Play />  Ta的作品
+            <Play /> Ta的作品
           </span>
         </TabsTrigger>
         <TabsTrigger
--- src/components/profile/video-tabs.tsx
+++ src/components/profile/video-tabs.tsx
@@ -15,12 +15,12 @@ const VideoTabs = () => {
   const dispatch = useDispatch();
   // console.log(defaultTab, "defaultab");
   return (
-    <Tabs defaultValue={defaultTab} className="py-5">
+    <Tabs defaultValue={defaultTab ? defaultTab : "liked"} className="py-5">
       <TabsList className="grid w-full grid-cols-3 z-[1600] bg-transparent sticky top-[100px] px-5">
         {/* {user?.token ? (
           <TabsTrigger
             className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2"
-            onClick={() => dispatch(setDefaultTab("upload"))}
+            onClick={() => dispatch(setDefaultTab ? defaultTab : "liked"("upload"))}
             value="upload"
           >
             <span className="flex items-center gap-1">
