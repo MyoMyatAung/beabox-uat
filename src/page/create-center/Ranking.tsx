e4b79c48f9f83f606f528d2c1f7718b086260b19
minthiha26 <minthiha.flar@gmail.com>
2025-03-19 12:38:24 +0630

    fix

--- src/page/create-center/Ranking.tsx
+++ src/page/create-center/Ranking.tsx
@@ -12,6 +12,7 @@ import { useEffect, useRef, useState } from "react";
 import InfinitLoad from "@/components/shared/infinit-load";
 import MyRank from "@/components/create-center/my-rank";
 import { useSelector } from "react-redux";
+import loader from "@/page/home/vod_loader.gif";
 
 const Ranking = () => {
   const [keyword, setKeyword] = useState("");
@@ -24,7 +25,7 @@ const Ranking = () => {
   const [rankingData, setRankingData] = useState<any>([]);
   const [hasMore, setHasMore] = useState(true);
   const [totalData, setTotalData] = useState<number>(0);
-  const { data, isLoading, refetch } = useGetTopCreatorQuery({
+  const { data, isLoading, refetch, isFetching } = useGetTopCreatorQuery({
     tag: keyword,
     page,
   });
@@ -95,6 +96,13 @@ const Ranking = () => {
 
   return (
     <div className="relative">
+      {isFetching ? (
+        <div className="fixed top-0 left-0 w-full h-screen bg-[#000000CC] flex justify-center items-center z-[3000]">
+          <img src={loader} alt="" className="w-20" />
+        </div>
+      ) : (
+        <></>
+      )}
       {showHeader ? (
         <div className="sticky top-0 w-full z-[9999] bg-[#16131C] pb-5">
           <TopNav center="热门创作者" />
