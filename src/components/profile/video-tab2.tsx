8496af2ac98291b1261c87aeeaa9e6a0ce3e5ebd
minthiha26 <minthiha.flar@gmail.com>
2025-03-23 20:58:20 +0630

    change tabs

--- src/components/profile/video-tab2.tsx
+++ src/components/profile/video-tab2.tsx
@@ -68,7 +68,6 @@
 
 // export default VideoTab2;
 
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Play } from "@/assets/profile";
 import { FaHeart } from "react-icons/fa";
@@ -82,9 +81,10 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
   const defaultTab2 = useSelector((state: any) => state.persist.defaultTab2);
   const dispatch = useDispatch();
   return (
-    <Tabs defaultValue={defaultTab2 ? defaultTab2 : "video"} className="my-5">
+    // <Tabs defaultValue={defaultTab2 ? defaultTab2 : "video"} className="my-5">
+    <Tabs defaultValue={"liked"} className="my-5">
       <TabsList className="grid w-full grid-cols-3 z-[1600] bg-transparent sticky top-[100px] px-5">
-        <TabsTrigger
+        {/* <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2 "
           value="video"
           onClick={() => dispatch(setDefaultTab2("video"))}
@@ -92,7 +92,7 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
           <span className="flex items-center gap-1">
             <Play /> Ta的作品
           </span>
-        </TabsTrigger>
+        </TabsTrigger> */}
         <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2 "
           value="liked"
