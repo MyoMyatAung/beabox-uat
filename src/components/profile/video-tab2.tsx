6fbffcb322b1f9a2a260eef0e5b9b1406cd1a2cb
minthiha26 <minthiha.flar@gmail.com>
2025-03-24 14:08:23 +0630

    fix tab

--- src/components/profile/video-tab2.tsx
+++ src/components/profile/video-tab2.tsx
@@ -81,10 +81,10 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
   const defaultTab2 = useSelector((state: any) => state.persist.defaultTab2);
   const dispatch = useDispatch();
   return (
-    // <Tabs defaultValue={defaultTab2 ? defaultTab2 : "video"} className="my-5">
-    <Tabs defaultValue={"liked"} className="my-5">
+    <Tabs defaultValue={defaultTab2 ? defaultTab2 : "video"} className="my-5">
+      {/* <Tabs defaultValue={"liked"} className="my-5"> */}
       <TabsList className="grid w-full grid-cols-3 z-[1600] bg-transparent sticky top-[100px] px-5">
-        {/* <TabsTrigger
+        <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2 "
           value="video"
           onClick={() => dispatch(setDefaultTab2("video"))}
@@ -92,7 +92,7 @@ const VideoTab2 = ({ id, visibility, showHeader }: any) => {
           <span className="flex items-center gap-1">
             <Play /> Ta的作品
           </span>
-        </TabsTrigger> */}
+        </TabsTrigger>
         <TabsTrigger
           className="text-[#888888] data-[state=active]:text-white data-[state=active]:bg-[#FFFFFF0A] rounded-full text-[12px] py-2 flex items-center gap-2 "
           value="liked"
