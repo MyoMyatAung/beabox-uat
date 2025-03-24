cb86e179d776f8623a2857e25743d05d4028dfb6
ryanreynold21 <aunkyawin2035@gmail.com>
2025-03-24 17:09:12 +0700

    check_carousel

--- src/page/application/Application.tsx
+++ src/page/application/Application.tsx
@@ -7,6 +7,10 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import ImageWithPlaceholder from "../explore/comp/imgPlaceHolder.tsx";
 import AsyncDecryptedImage from "@/utils/asyncDecryptedImage.tsx";
+import "swiper/css";
+import "swiper/css/autoplay"; // Import Swiper autoplay styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules"; // Import Swiper's autoplay module
 
 const Application: React.FC<any> = () => {
   const [ad, setAd] = useState([]);
@@ -20,8 +24,12 @@ const Application: React.FC<any> = () => {
     setAutoPlay(true); // Activate autoPlay once on mount
   }, []);
 
-  const handleOnChange = (index: number) => {
-    setSelectedIndex(index);
+  // const handleOnChange = (index: number) => {
+  //   setSelectedIndex(index);
+  // };
+
+  const handleOnChange = (swiper: any) => {
+    setSelectedIndex(swiper.realIndex);
   };
 
   // Update carousel only when applicationData changes
@@ -88,7 +96,7 @@ const Application: React.FC<any> = () => {
         {!isLoading && applicationData && (
           <>
             {/* Carousel Section */}
-            <Carousel
+            {/* <Carousel
               showThumbs={false}
               showArrows={false}
               showStatus={false}
@@ -119,9 +127,9 @@ const Application: React.FC<any> = () => {
                   />
                 </a>
               ))}
-            </Carousel>
+            </Carousel> */}
             {/* Custom Dots */}
-            <ul className="flex justify-center items-center gap-[4px] w-full mt-2">
+            {/* <ul className="flex justify-center items-center gap-[4px] w-full mt-2">
               {ad.map((_, dotIndex) => (
                 <li
                   key={dotIndex}
@@ -133,7 +141,55 @@ const Application: React.FC<any> = () => {
                   tabIndex={0}
                 ></li>
               ))}
-            </ul>
+            </ul> */}
+            <div className=" relative py-[20px]">
+              <Swiper
+                className=""
+                slidesPerView={1.5}
+                spaceBetween={110}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                modules={[Autoplay]}
+                onSlideChange={handleOnChange}
+              >
+                {ad.map((cc: any, index: number) => (
+                  <SwiperSlide className=" w-full rounded-[12px]" key={index}>
+                    <a
+                      href={cc.url}
+                      target="_blank"
+                      className={`flex rounded-[12px] justify-center w-full items-center px-[8px] flex-col relative transition-all duration-300 `}
+                    >
+                      <div className=" w-[332px] h-[162px] px-2 overflow-hidden rounded-[12px]">
+                        <img
+                          className={`object-cove w-full h-full transition-all rounded-[12px] duration-300 ${
+                            selectedIndex === index
+                              ? "p-0 m-0 opacity-100"
+                              : "p-2 opacity-70 "
+                          }`}
+                          src={cc.image}
+                          alt={`Slide ${index + 1}`}
+                        />
+                      </div>
+                    </a>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+
+              <ul className="flex justify-center items-center gap-[10px] w-full  mt-2 absolute bottom-0 left-0">
+                {ad.map((_, dotIndex) => (
+                  <li
+                    key={dotIndex}
+                    className={`w-[6px] h-[6px] rounded-full ${
+                      selectedIndex === dotIndex ? "bg-white" : "bg-[#888]"
+                    }`}
+                    onClick={() => handleOnChange(dotIndex)}
+                    role="button"
+                    tabIndex={0}
+                  ></li>
+                ))}
+              </ul>
+            </div>
 
             {/* Header Section */}
             <div className="mt-[20px]">
