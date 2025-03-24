11e5d2ff8cc0c7a73663a345bc19f0e84e358866
minthiha26 <minthiha.flar@gmail.com>
2025-03-24 16:07:14 +0630

    add avatar

---
+++ src/components/avatar/avatar-upload.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AvatarUpload = ({ setShowAvatar }: any) => {
+  return <div onClick={() => setShowAvatar(true)}>AvatarUpload</div>;
+};
+
+export default AvatarUpload;
---
+++ src/components/avatar/avatars.tsx
@@ -0,0 +1,74 @@
+import ImageWithPlaceholder from "@/page/explore/comp/imgPlaceHolder";
+import { useGetAvatarListQuery } from "@/store/api/createCenterApi";
+import { X } from "lucide-react";
+import AvatarImage from "./avatar-image";
+
+const Avatars = ({ setShowAvatar }: any) => {
+  const { data } = useGetAvatarListQuery("");
+  console.log(data?.data, "avatars");
+  return (
+    <div className="bg-[#000000CC] w-full flex justify-center items-center h-screen absolute top-0 left-0 z-50">
+      <div className="bg-[#16131C] rounded-[22px] w-[90%] h-[90%] flex flex-col justify-between">
+        <div className="">
+          <TopBar setShowAvatar={setShowAvatar} />
+        </div>
+        <div className="flex-1 overflow-hidden overflow-y-scroll hide-sb">
+          <div className="px-5 flex flex-col gap-5">
+            {data?.data?.map((list: any, index: any) => (
+              <div key={index}>
+                <h1
+                  className={`text-[14px] mb-4 ${
+                    list?.is_available ? "text-white" : "text-[#888888]"
+                  }`}
+                >
+                  {list?.level}
+                </h1>
+                <div className="flex justify-between">
+                  {list?.list?.map((item: any) => (
+                    <div className="" key={item?.image}>
+                      <AvatarImage
+                        className={`w-[60px] h-[60px] rounded-full ${
+                          !list?.is_available ? "brightness-50" : ""
+                        }`}
+                        src={item.image}
+                        width={"60px"}
+                        height={"60px"}
+                        alt={""}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="p-5">
+          <button className="gradient-bg w-full text-[14px] py-4 rounded-[16px]">
+            保存
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Avatars;
+
+const TopBar = ({ setShowAvatar }: any) => {
+  return (
+    <div className="flex justify-between items-center p-5">
+      <div className=""></div>
+      <div className="">
+        <p className="text-[18px]">选择头像</p>
+      </div>
+      <div className="">
+        <button
+          onClick={() => setShowAvatar(false)}
+          className="bg-[#FFFFFF0A] p-1 rounded-full"
+        >
+          <X size={15} />
+        </button>
+      </div>
+    </div>
+  );
+};
