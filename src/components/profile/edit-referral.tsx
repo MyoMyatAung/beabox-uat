fc4407d315475538ae0d10c4a3b6bb96263a15f6
minthiha26 <minthiha.flar@gmail.com>
2025-04-13 16:48:29 +0630

    fix er

--- src/components/profile/edit-referral.tsx
+++ src/components/profile/edit-referral.tsx
@@ -1,12 +1,18 @@
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 import { useSelector } from "react-redux";
 import { useChangeReferralCodeMutation } from "@/store/api/profileApi";
-const EditReferral = ({ referral_code }: any) => {
+import Loader from "../shared/loader";
+const EditReferral = ({
+  referral_code,
+  showAlertHandler,
+  refetchHandler,
+}: any) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
   const [value, setValue] = useState("");
   const user = useSelector((state: any) => state?.persist?.user);
   const [changeRerralCode, { data, isLoading }] =
@@ -15,35 +21,57 @@ const EditReferral = ({ referral_code }: any) => {
   const onSubmitHandler = async (e: any) => {
     e.preventDefault();
     await changeRerralCode({ referral_code: value });
+    await refetchHandler();
     setIsOpen(false);
   };
 
-  // console.log(data, "referral code data");
+  useEffect(() => {
+    setValue(referral_code);
+  }, [isOpen]);
+  // console.log(referral_code, "referral code data");
   return (
     <Drawer open={isOpen} onOpenChange={() => setIsOpen(true)}>
-      <div className="text-[14px] flex items-center justify-between">
-        <h1>邀请码</h1>
-        <DrawerTrigger asChild>
+      {referral_code ? (
+        <div
+          onClick={showAlertHandler}
+          className="text-[14px] flex items-center justify-between"
+        >
+          <h1>邀请码</h1>
           <p className="flex items-center gap-1 text-[#888]">
             {referral_code}
             <FaAngleRight />
           </p>
+        </div>
+      ) : (
+        <DrawerTrigger asChild>
+          <div className="text-[14px] flex items-center justify-between">
+            <h1>邀请码</h1>
+            <p className="flex items-center gap-1 text-[#888]">
+              {referral_code}
+              <FaAngleRight />
+            </p>
+          </div>
         </DrawerTrigger>
-      </div>
+      )}
       <DrawerContent className="border-0">
+        {isLoading ? <Loader /> : <></>}
+
         <div className="w-full h-screen px-5">
           <div className="flex justify-between items-center py-5">
             <button onClick={() => setIsOpen(false)}>
               <FaAngleLeft size={22} />
             </button>
-            <p className="text-[16px]">Referral Code</p>
+            <p className="text-[16px]">我的推广达人</p>
             <div></div>
           </div>
           <form onSubmit={onSubmitHandler}>
+            <label htmlFor="" className="text-[14px] text-[#888] pt-10">
+              我的推广达人
+            </label>
             <div className="relative">
               <input
                 className="w-full bg-transparent border-0 border-b py-3 outline-0 border-[#888]"
-                placeholder="Referral Code"
+                placeholder="输入邀请码"
                 onChange={(e: any) => setValue(e.target.value)}
                 value={value}
                 // defaultValue={referral_code}
@@ -63,7 +91,8 @@ const EditReferral = ({ referral_code }: any) => {
                   : "bg-[#FFFFFF0A] hover:bg-[#FFFFFF0A]"
               } bg-[#FFFFFF0A]   mt-10 rounded-xl`}
             >
-              {isLoading ? "loading..." : "Save"}
+              保存
+              {/* {isLoading ? "loading..." : "Save"} */}
             </Button>
           </form>
         </div>
