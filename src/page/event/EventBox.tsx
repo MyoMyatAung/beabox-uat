46ed7810ef11f3ba59176bbfe1a24c66fa13e9b4
ryanreynold21 <aunkyawin2035@gmail.com>
2025-04-25 17:33:20 +0700

    add_animation

--- src/page/event/EventBox.tsx
+++ src/page/event/EventBox.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./event.css";
 import logo from "./img/logoBox.png";
 import light from "./img/light.json";
+import card from "./img/card.json";
 import bg from "./img/bg.png";
 import AsyncDecryptedImage from "@/utils/asyncDecryptedImage";
 import Animation from "./Animation";
@@ -14,12 +15,15 @@ const EventBox: React.FC<EventBoxProps> = ({ eventData }) => {
   console.log(eventData);
   return (
     <div className="h-screen bg-black/80 w-screen flex flex-col gap-[20px] justify-center items-center fixed top-0 z-[9999]">
-      <div className=" absolute z-[-2] top-[80px]">
+      <div className=" absolute z-[-2] top-[150px]">
         <Animation  animate={light} />
       </div>
       <div className=" w-[300px] h-[450px] flex flex-col justify-between items-center event_bo">
-        <img className=" absolute z-[-1]" src={bg} alt="" />
-        <div className=" w-full h-full py-[30px] flex flex-col justify-between items-center">
+        {/* <img className=" absolute z-[-1]" src={bg} alt="" /> */}
+        <div className=" absolute z-[-1]">
+        <Animation  animate={card} />
+        </div>
+        <div className=" w-full h-full pt-[70px] pb-[30px] flex flex-col justify-between items-center">
           <img className=" w-[210px] h-[70pxx]" src={logo} alt="" />
           <div className=" flex flex-col justify-center items-center px-[30px]">
             <AsyncDecryptedImage
