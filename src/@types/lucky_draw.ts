69a1e84f9c8f3f3f59c4f646265ce908201c7535
SoeHtun <soehtuncu@gmail.com>
2025-04-30 20:26:10 +0700

    remove countdown & show date

--- src/@types/lucky_draw.ts
+++ src/@types/lucky_draw.ts
@@ -7,6 +7,9 @@ export type EventDetail = {
   cumulative_earnings: string;
   this_month_earnings: string;
   last_month_earnings: string;
+  event_start_time: number;
+  event_end_time:number;
+  server_timezone: string
 }
 
 export interface CurrentEventResponse {
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -83,7 +83,7 @@ const RootLayout = ({ children }: any) => {
   const { data: currentEventData } = useGetCurrentEventQuery("");
   const [triggerGetEventDetails] = useLazyGetEventDetailsQuery();
   const showAnimation = useSelector((state: RootState) => state.event.isShowAnimation);
-  const currentDuration = useSelector((state: RootState) => state.event.duration);
+  const currentDuration = useSelector((state: RootState) => state.event.event_start_time);
 
   useEffect(() => {
     if(showAd && showAlert && isOpen && !showLanding) {
@@ -226,19 +226,18 @@ const RootLayout = ({ children }: any) => {
 
     const eventId = currentEventData?.data?.id;
     if (!eventId) return;
-
     // Only fetch event details if duration is 0
-    if (currentDuration <= 0) {
+    // if (currentDuration <= 0) {
       try {
         const eventDetails = await triggerGetEventDetails(eventId).unwrap();
         dispatch(setEventDetail(eventDetails.data));
-        if (eventDetails.data?.duration) {
-          dispatch(setDuration(eventDetails.data.duration));
+        if (eventDetails.data?.event_start_time) {
+          dispatch(setDuration(eventDetails.data.event_start_time));
         }
       } catch (error) {
         console.error('Failed to fetch event details:', error);
       }
-    }
+    // }
 
     navigate(`/events/lucky-draw/${eventId}`);
   };
