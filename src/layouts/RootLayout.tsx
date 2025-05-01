996006c23521a3cfbffe56fcd93c9985a81f0f14
hlaing-dev <hlaingmin1235145@gmail.com>
2025-05-01 18:22:05 +0700

    update

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -66,7 +66,7 @@ const RootLayout = ({ children }: any) => {
   const [isOpenNew, setIsOpenNew] = useState(false);
   const [code, setCode] = useState("");
   const [newData, setnewData] = useState(null);
-  const user = useSelector((state: any) => state.persist.user); 
+  const user = useSelector((state: any) => state.persist.user);
 
   const { data: eventData } = useGetUserByReferalQuery(
     { referral_code: referCode }, // or safely cast if you're confident it's a string
