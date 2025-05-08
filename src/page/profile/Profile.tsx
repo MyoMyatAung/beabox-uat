9dfb673fd5bfe7b2ad44f4b9cbd38e4107e04ad0
minthiha26 <minthiha.flar@gmail.com>
2025-05-08 12:38:00 +0630

    fix

--- src/page/profile/Profile.tsx
+++ src/page/profile/Profile.tsx
@@ -29,6 +29,7 @@ const Profile = () => {
   const { data, isLoading, refetch } = useGetMyOwnProfileQuery("", {
     skip: !user,
   });
+  console.log(data, "data for profile");
   const progressData = data?.data?.level_progress;
   const [show, setShow] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
