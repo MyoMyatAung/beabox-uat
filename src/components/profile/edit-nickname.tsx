33d51ba65d610338416cf01bc5db43769f933ab5
minthiha26 <minthiha.flar@gmail.com>
2025-05-11 12:28:00 +0630

    fix

--- src/components/profile/edit-nickname.tsx
+++ src/components/profile/edit-nickname.tsx
@@ -65,15 +65,15 @@ const EditNickName = ({
 
   return (
     <Drawer open={isOpen} onOpenChange={handleOpenChange}>
-      <DrawerTrigger>
+      {/* <DrawerTrigger>
         <div className="text-[14px] flex items-center justify-between">
           <h1>昵称</h1>
           <p className="flex items-center gap-1 text-[#888]">
             {nickname} <FaAngleRight />
           </p>
         </div>
-      </DrawerTrigger>
-      {/* <div>
+      </DrawerTrigger> */}
+      <div>
         <div
           onClick={() => showErrorToast()}
           className="text-[14px] flex items-center justify-between"
@@ -83,7 +83,7 @@ const EditNickName = ({
             {nickname} <FaAngleRight />
           </p>
         </div>
-      </div> */}
+      </div>
       <DrawerContent className="border-0" style={{ height: vh }}>
         {isLoading ? <Loader /> : <></>}
         <div className="w-full px-5 bg-[#16131C]">
