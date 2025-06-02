434f6defc88e4d8cc195cb13b50ee5206cf99fd0
hlaing-dev <hlaingmin1235145@gmail.com>
2025-06-02 15:56:16 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       },
       workbox: {
         navigateFallback: "/index.html",
-        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024, // 6 MB
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
       },
       devOptions: {
         enabled: true
@@ -27,7 +27,7 @@ export default defineConfig({
       filename: 'sw.js',
       base: '/',
       injectManifest: {
-        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024, // 6 MB
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
       }
     }),
     react(),
