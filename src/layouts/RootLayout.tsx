7c903c601cf34de77b80eb1ac5e6fecedc72269e
hlaing-dev <hlaingmin1235145@gmail.com>
2025-06-13 23:47:51 +0700

    update

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -35,6 +35,7 @@ import EventBox from "@/page/event/EventBox";
 import RegisterDrawer from "@/components/profile/auth/register-drawer";
 import { EventDetail } from "@/@types/lucky_draw";
 import DEventBox from "@/page/event/dragon/DEventBox";
+import { motion, AnimatePresence } from "framer-motion";
 
 // Function to check if the app is running in a WebView
 function isWebView() {
@@ -348,6 +349,7 @@ const RootLayout = ({ children }: any) => {
     return <Landing onComplete={handleLandingComplete} />;
   }
   const handleAnimationClick = async () => {
+    console.log('currentEventData is=>', currentEventData);
     const eventId = currentEventData?.data?.id;
     if (!eventId) return;
 
@@ -411,113 +413,142 @@ const RootLayout = ({ children }: any) => {
   }
   return (
     <>
-    <div style={{ height: "calc(100dvh - 95px);" }}>
-      {children}
+      <div style={{ height: "calc(100dvh - 95px);" }}>
+        {children}
 
-      {event && !box && !isOpenNew && !user && (
-        <DEventBox
-          setshownextBox={setshownextBox}
-          shownextBox={shownextBox}
-          eventData={eventData}
-          setBox={setBox}
-          referCode={referCode}
-          isOpen={isOpenNew}
-          setIsOpen={setIsOpenNew}
-          setCode={setCode}
-          newData={newData}
-          setnewData={setnewData}
-          setEvent={setEvent}
-        />
-      )}
-      {isOpenNew && (
-        <RegisterDrawer
-          isOpen={isOpenNew}
-          setIsOpen={setIsOpenNew}
-          code={referCode}
-          geetest_id={code}
-        />
-      )}
-
-      {showAd && !event && (
-        <PopUp
-          setShowAd={setShowAd}
-          setShowAlert={setShowAlert}
-          isBrowser={isBrowser}
-          onComplete={handleAdComplete}
-        />
-      )}
-      {!showAd && showAlert && isBrowser && jumpUrl && showDialog && !event && (
-        <AlertRedirect
-          event={event}
-          setShowAlert={setShowAlert}
-          app_download_link={jumpUrl}
-        />
-      )}
-      {isOpen ? <AuthDrawer /> : <></>}
-
-      <AlertToast />
-      <div className="fixed bottom-0 left-0 w-full z-[1600]">
-        <BottomNav />
-      </div>
-
-      {!showAd &&
-        // !showAlert &&
-        !isOpen &&
-        location.pathname === "/" &&
-        !event &&
-        showAnimation &&
-        currentTab === 2 &&
-        !hideBar &&
-        !userHasClosedAnimation && (
-          <>
-          {
-            showEvent && 
-            <>
-            <div className="fixed bottom-[24rem] left-2 z-[9999] rounded-full p-2">
-              <div className="relative">
-                <AnimationLoader
-                  animationData={countdownAnimation}
-                  width={100}
-                  height={100}
-                  onClick={handleAnimationClick}
-                />
-              </div>
-            </div>
-            <div className="fixed bottom-[19rem] left-4 z-[9999] rounded-full p-2">
-              <div className="relative">
-                <AnimationLoader
-                  animationData={luckySpinAnimation}
-                  width={80}
-                  height={80}
-                  onClick={()=>setShowLuckySpin(true)}
-                />
-              </div>
-            </div>
-            </>
-}
-            <div className="fixed bottom-[12rem] left-1 z-[9999] rounded-full p-2">
-              <div className="relative">
-                <button
-                  className="absolute top-1 right-2 bg-red rounded-full w-5 h-5 flex items-center justify-center text-black z-[10000]"
-                  onClick={() => {
-                    dispatch(setAnimation(false));
-                    setUserHasClosedAnimation(true);
-                    sessionStorage.setItem("animationClosed", "true");
-                  }}
-                >
-                  <img src={CloseSvg} />
-                </button>
-                <AnimationLoader
-                  animationData={fabAnimation}
-                  width={100}
-                  height={100}
-                  onClick={()=>setShowEvent(!showEvent)}
-                />
-              </div>
-            </div>
-          </>
+        {event && !box && !isOpenNew && !user && (
+          <DEventBox
+            setshownextBox={setshownextBox}
+            shownextBox={shownextBox}
+            eventData={eventData}
+            setBox={setBox}
+            referCode={referCode}
+            isOpen={isOpenNew}
+            setIsOpen={setIsOpenNew}
+            setCode={setCode}
+            newData={newData}
+            setnewData={setnewData}
+            setEvent={setEvent}
+          />
         )}
-    </div>
+        {isOpenNew && (
+          <RegisterDrawer
+            isOpen={isOpenNew}
+            setIsOpen={setIsOpenNew}
+            code={referCode}
+            geetest_id={code}
+          />
+        )}
+
+        {showAd && !event && (
+          <PopUp
+            setShowAd={setShowAd}
+            setShowAlert={setShowAlert}
+            isBrowser={isBrowser}
+            onComplete={handleAdComplete}
+          />
+        )}
+        {!showAd &&
+          showAlert &&
+          isBrowser &&
+          jumpUrl &&
+          showDialog &&
+          !event && (
+            <AlertRedirect
+              event={event}
+              setShowAlert={setShowAlert}
+              app_download_link={jumpUrl}
+            />
+          )}
+        {isOpen ? <AuthDrawer /> : <></>}
+
+        <AlertToast />
+        <div className="fixed bottom-0 left-0 w-full z-[1600]">
+          <BottomNav />
+        </div>
+
+        {!showAd &&
+          // !showAlert &&
+          !isOpen &&
+          location.pathname === "/" &&
+          !event &&
+          showAnimation &&
+          currentTab === 2 &&
+          !hideBar &&
+          !userHasClosedAnimation && (
+            <>
+              <AnimatePresence>
+                {showEvent && (
+                  <>
+                    <motion.div
+                      key="countdown"
+                      className="fixed bottom-[22rem] left-2 z-[9999] rounded-full p-2"
+                      initial={{ y: 100, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 100, opacity: 0 }}
+                      transition={{
+                        type: "spring",
+                        damping: 20,
+                        stiffness: 300,
+                      }}
+                    >
+                      <div className="relative">
+                        <AnimationLoader
+                          animationData={countdownAnimation}
+                          width={110}
+                          onClick={handleAnimationClick}
+                        />
+                      </div>
+                    </motion.div>
+
+                    <motion.div
+                      key="luckySpin"
+                      className="fixed bottom-[19rem] left-4 z-[9999] rounded-full p-2"
+                      initial={{ y: 100, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 100, opacity: 0 }}
+                      transition={{
+                        type: "spring",
+                        damping: 20,
+                        stiffness: 300,
+                        delay: 0.1,
+                      }}
+                    >
+                      <div className="relative">
+                        <AnimationLoader
+                          animationData={luckySpinAnimation}
+                          width={85}
+                          height={100}
+                          onClick={() => setShowLuckySpin(true)}
+                        />
+                      </div>
+                    </motion.div>
+                  </>
+                )}
+              </AnimatePresence>
+              <div className="fixed bottom-[12rem] left-1 z-[9999] rounded-full p-2">
+                <div className="relative">
+                  <button
+                    className="absolute top-1 right-2 bg-red rounded-full w-5 h-5 flex items-center justify-center text-black z-[10000]"
+                    onClick={() => {
+                      dispatch(setAnimation(false));
+                      setUserHasClosedAnimation(true);
+                      sessionStorage.setItem("animationClosed", "true");
+                    }}
+                  >
+                    <img src={CloseSvg} />
+                  </button>
+                  <AnimationLoader
+                    animationData={fabAnimation}
+                    width={100}
+                    height={100}
+                    onClick={() => setShowEvent(!showEvent)}
+                  />
+                </div>
+              </div>
+            </>
+          )}
+      </div>
     </>
   );
 };
