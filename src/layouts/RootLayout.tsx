72b7b0113b1b2a007a7a7771449b36a873675da8
hlaing-dev <hlaingmin1235145@gmail.com>
2025-06-17 12:16:38 +0700

    update

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -107,9 +107,9 @@ const RootLayout = ({ children }: any) => {
 
   useEffect(() => {
     // dev
-    const webUrl = "http://localhost:5001";
+    // const webUrl = "http://localhost:5001";
     // prod
-    // const webUrl = currentEventData?.data.filter((x: any) => x.type === 'spin-wheel')[0]?.web_url;
+    const webUrl = currentEventData?.data.filter((x: any) => x.type === 'spin-wheel')[0]?.web_url;
     setLuckySpinWebUrl(webUrl);
     if (showAd && showAlert && isOpen && !showLanding) {
       dispatch(setAnimation(false));
